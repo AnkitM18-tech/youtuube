4d6f472033ca1fa9683bcc8265d16a38e03210ac
Ankit Kumar Mohanty <ankitmohanty1804@gmail.com>
2023-01-18 18:41:12 +0530

    Feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -31,7 +31,7 @@ const Feed = () => {
           variant="body2"
           sx={{ mt: 1.5, color: "#fff" }}
         >
-          Copyright 2023 Youtuube
+          Copyright © 2023 Youtuube
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
